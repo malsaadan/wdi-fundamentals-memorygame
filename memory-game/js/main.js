b3fbc5dd3a449a8ca59cefc7f5f000803df31543
Mashael Mohammed <mashaelmohammed@Mashaels-MacBook-Pro.local>
2020-01-14 05:34:50 +0300

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,14 +1,25 @@
+
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+function checkForMatch(){
 
-if(cardsInPlay.length === 2){
 	if(cardsInPlay[0] === cardsInPlay[1])
-		alert('You found a match!');
+		console.log('You found a match!');
 	else
-		alert('Sorry, try again.')
+		console.log('Sorry, try again.');
+	
 }
+
+function flipCard(cardId){
+
+	console.log('User flipped '+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if(cardsInPlay.length === 2)
+		checkForMatch();
+}
+
+
+flipCard(0);
+flipCard(2);
